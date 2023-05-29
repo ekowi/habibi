89fffa85024344f6d804c0c063e535109ebff713
ekowi <greget103@gmail.com>
2023-05-29 23:04:48 +0700

    ready to deploy

---
+++ app/controllers/habibi_controller.rb
@@ -0,0 +1,13 @@
+class HabibiController < ApplicationController
+    def index
+    end
+
+    def products
+    end
+
+    def about
+    end
+
+    def store
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/products", to: "habibi#products"
+  get "/about", to: "habibi#about"
+  get "/store", to: "habibi#store"
+
+
   # Defines the root path route ("/")
   # root "articles#index"
+  root "habibi#index"
 end
